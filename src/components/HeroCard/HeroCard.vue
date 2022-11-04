2935dae92d9c2cd4058566da0cec6d70bec52283
danix86 <daniel.daxer34@gmail.com>
2022-11-04 13:12:48 +0100

    made some stuff

--- src/components/HeroCard/HeroCard.vue
+++ src/components/HeroCard/HeroCard.vue
@@ -1,10 +1,13 @@
 <script setup>
     const props = defineProps(['hero']);
+    function showBiography() {
+        console.log(props.hero.biography);
+    }
 </script>
 
 <template>
     <div grid class="heroCard">
-        <HeroImage :imageUrl="hero.images.md"/>
+        <HeroImage :imageUrl="hero.images.md"  @showBiography="showBiography"/>
         <HeroStats :name="hero.name" :stats="hero.powerstats" :alignment="hero.biography.alignment"/>
     </div>
 </template>
--- src/components/HeroCard/HeroStats.vue
+++ src/components/HeroCard/HeroStats.vue
@@ -3,5 +3,20 @@
 </script>
 
 <template>
+    <div :class="alignment">
     <h1>{{name}}</h1>
+    <StatField v-for="(value, key) in stats" :key="key" :skill="key" :value="value">{{key}}: {{value}}</StatField>
+    </div>
 </template>
+
+<style scoped>
+.bad {
+    background-color: red;
+}
+.good {
+    background-color: green;
+}
+.neutral {
+    background-color: yellow;
+}
+</style>
