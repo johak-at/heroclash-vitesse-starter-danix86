52f7fb57404a1443552c8bf457841c6089b8ff35
danix86 <daniel.daxer34@gmail.com>
2022-11-17 20:09:36 +0100

    dings

--- src/components/HeroCard/HeroCard.vue
+++ src/components/HeroCard/HeroCard.vue
@@ -7,7 +7,7 @@
     <div grid class="heroCard">
         <HeroImage :imageUrl="hero.images.md"  @showBiography="showBiography = true"/>
         <HeroStats :name="hero.name" :stats="hero.powerstats" :alignment="hero.biography.alignment"/>
-        <HeroModal v-if="showBiography" @hideBiography="showBiography = false"></HeroModal>
+        <HeroModal v-if="showBiography" @hideBiography="showBiography = false" :biography="hero.biography" :connections="hero.connections" :appearance="hero.appearance" :work="hero.work"></HeroModal>
     </div>
 </template>
 
--- src/components/HeroCard/HeroModal.vue
+++ src/components/HeroCard/HeroModal.vue
@@ -1,22 +1,37 @@
 <script setup>
 const emits = defineEmits(["hideBiography"])
+const props = defineProps(["biography", "connections", "appearance", "work"])
 </script>
 
 <template>
 <div class = "modal" @click="emits('hideBiography')">
-    <ul>
-        <li></li>
+    <ul v-for="(stuff, key) in biography" :key="key">
+        <li>{{key}}: {{stuff}}</li>
+    </ul>
+    <br />
+    <ul v-for="(stuff, key) in appearance" :key="key">
+        <li>{{key}}: {{stuff}}</li>
+    </ul>
+    <br />
+    <ul v-for="(stuff, key) in connections" :key="key">
+        <li>{{key}}: {{stuff}}</li>
+    </ul>
+    <br />
+    <ul v-for="(stuff, key) in work" :key="key">
+        <li>{{key}}: {{stuff}}</li>
     </ul>
 </div>
 </template>
 
 <style scoped>
 .modal {
+    text-align: left;
+    padding: 3rem;
     position: fixed;
     top: 0;
     left: 0;
     width: 100vw;
     height: 100vh;
-    background-color: pink;
+    background-color: grey;
     }
 </style>
